2ad61ff63e8492ab4da44b763cb32543938103ad
Pau Gil Corretger <68238497+paugi13@users.noreply.github.com>
2022-05-07 19:47:15 +0200

    No relevant changes

--- main.m
+++ main.m
@@ -92,7 +92,7 @@ T_plot(:, nod_reforc_2(end)+2:end) = T(:, nod_poma(1):end);
 [X,Y] = meshgrid(coord_total,y);
 
 figure
-surf(X,Y, zeros(size(X)),T_plot(100,2:end), 'edgecolor','none');
+surf(X,Y, zeros(size(X)),T_plot(500,2:end), 'edgecolor','none');
 colorbar
 xlabel('x [m]');
 ylabel('y [m]');
